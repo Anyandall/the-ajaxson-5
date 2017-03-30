4b305113180a202eb4dc0667b5d931182b99390b
Sean Dennis <carter.swychord@gmail.com>
2017-03-30 14:27:33 -0500

    connected code to new form, submit and captcha work with correct data and logs event with incorrect value entered

--- request-gif.js
+++ request-gif.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     // register our function as the "callback" to be triggered by the form's submission event
-    $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
+    $("#validation").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
 });
 
 
@@ -17,54 +17,59 @@ function fetchAndDisplayGif(event) {
     // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
     // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
     event.preventDefault();
-    setGifLoadedStatus(false);
-    $("#feedback").text("Loading...");
+    if ($('#valid').val() == 5) {
+      setGifLoadedStatus(false);
+      $("#feedback").text("Loading...");
 
 
-    // get the user's input text from the DOM
-    var searchQuery = $('#tag').val(); // TODO should be e.g. "dance"
-    console.log(searchQuery);
-    console.log("test searchQuery value above")
+      // get the user's input text from the DOM
+      var searchQuery = $('#tag').val(); // TODO should be e.g. "dance"
+      console.log(searchQuery);
+      console.log("test searchQuery value above")
 
-    // configure a few parameters to attach to our request
-    var params = {
-        root: "http://api.giphy.com/v1/gifs/search",
-        api_key: "dc6zaTOxFJmzC",
-        tag : "jackson+5+" + searchQuery // TODO should be e.g. "jackson 5 dance"
-    };
-
-
-    // make an ajax request for a random GIF
-    $.ajax({
-        url: params.root + "?q=" + params.tag + "&api_key=" + params.api_key + "&limit=25", // TODO where should this request be sent?
-        data: params, // attach those extra parameters onto the request
-        success: function(response) {
-            // if the response comes back successfully, the code in here will execute.
+      // configure a few parameters to attach to our request
+      var params = {
+          root: "http://api.giphy.com/v1/gifs/search",
+          api_key: "dc6zaTOxFJmzC",
+          tag : "jackson+5+" + searchQuery // TODO should be e.g. "jackson 5 dance"
+      };
 
 
-            // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
-            console.log("we received a response!");
-            console.log(response);
-            var item = response.data[Math.floor(Math.random()*response.data.length)].id;
+      // make an ajax request for a random GIF
+      $.ajax({
+          url: params.root + "?q=" + params.tag + "&api_key=" + params.api_key + "&limit=25", // TODO where should this request be sent?
+          data: params, // attach those extra parameters onto the request
+          success: function(response) {
+              // if the response comes back successfully, the code in here will execute.
 
-            // TODO
-            // 1. set the source attribute of our image to the image_url of the GIF
-            //$("#gif").attr("src", response.data[0].url + "/giphy.gif");
-            //$("#gif").attr("src", "https://www.w3schools.com/css/trolltunga.jpg");
-            //$("#gif").attr("src", "https://media.giphy.com/media/gw3IWyGkC0rsazTi/giphy.gif");
-            $("#gif").attr("src", "https://media.giphy.com/media/" + item + "/giphy.gif")
-            // 2. hide the feedback message and display the image
 
-            setGifLoadedStatus(true)
-        },
-        error: function() {
-            // if something went wrong, the code in here will execute instead of the success function
+              // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
+              console.log("we received a response!");
+              console.log(response);
+              var random_url_id = response.data[Math.floor(Math.random()*response.data.length)].id;
 
-            // give the user an error message
-            $("#feedback").text("Sorry, could not load GIF. Try again!");
-            setGifLoadedStatus(false);
-        }
-    });
+              // TODO
+              // 1. set the source attribute of our image to the image_url of the GIF
+              //$("#gif").attr("src", response.data[0].url + "/giphy.gif");
+              //$("#gif").attr("src", "https://www.w3schools.com/css/trolltunga.jpg");
+              //$("#gif").attr("src", "https://media.giphy.com/media/gw3IWyGkC0rsazTi/giphy.gif");
+              $("#gif").attr("src", "https://media.giphy.com/media/" + random_url_id + "/giphy.gif")
+              // 2. hide the feedback message and display the image
+
+              setGifLoadedStatus(true)
+          },
+          error: function() {
+              // if something went wrong, the code in here will execute instead of the success function
+
+              // give the user an error message
+              $("#feedback").text("Sorry, could not load GIF. Try again!");
+              setGifLoadedStatus(false);
+          }
+      });
+    } else {
+      console.log("didn't complete captcha")
+    }
+
 
     // TODO
     // give the user a "Loading..." message while they wait
